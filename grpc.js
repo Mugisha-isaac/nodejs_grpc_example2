98a3332c5d73f0d1851f88fb414c7fbffb50acf2
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-16 11:52:12 +0200

    [feat: grpcs.js] packageDefinition

--- grpc.js
+++ grpc.js
@@ -4,4 +4,14 @@ const grpc = require('@grpc/grpc-js');
 const PROTO_PATH = __dirname + "/protos/user.proto";
 
 
-// const packageDefinition
+const packageDefinition = protoLoader.loadSync(PROTO_PATH,{
+    keepCase: true,
+    longs:String,
+    defaults:true,
+    oneofs:true
+});
+
+
+const user_proto = grpc.loadPackageDefinition(packageDefinition);
+
+startGrpcServer();
