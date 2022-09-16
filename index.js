19c699dd8842fafae29e1186a1d1b15ca6b93067
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-16 11:36:18 +0200

    [feat:index.js] starting grpc server

---
+++ index.js
@@ -0,0 +1,8 @@
+const PROTO_PATH = __dirname + "/protos/user.proto";
+const grpc = require("@grpc/grpc-js");
+const server = new grpc.Server();
+
+exports.startGrpcServer = function(){
+    
+};
+exports.getGrpcServer = function(){};
