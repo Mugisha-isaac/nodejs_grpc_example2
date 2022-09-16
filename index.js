da3d0402fd2ed30b21113c253947cf3116ad8a5a
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-16 11:42:36 +0200

    [feat: index.js] startGrpcServer method

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const grpc = require("@grpc/grpc-js");
 const server = new grpc.Server();
 
 exports.startGrpcServer = function(){
-    
+    server.bindAsync(
+        "127.0.0.1:50050",
+        grpc.ServerCredentials.createInsecure(),
+        (error,port)=>{
+            if(error) console.log(error);
+            else{
+
+                server.start();
+                console.log(`server is running at 127.0.0.1:${port}`);
+            } 
+        }
+    )
 };
 exports.getGrpcServer = function(){};
